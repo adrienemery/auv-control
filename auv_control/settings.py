fcf4de5b874151d93c8d1005c522b535db9ab40c
Adrien Emery <adrien.emery@gmail.com>
2016-08-07 14:50:31 -0700

    use json renderer

--- auv_control/settings.py
+++ auv_control/settings.py
@@ -131,3 +131,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
+
+
+# DRF
+
+REST_FRAMEWORK = {
+    'DEFAULT_RENDERER_CLASSES': (
+        'rest_framework.renderers.JSONRenderer',
+    )
+}
