3e2d1cb888e0272226f3719d23e96435a962308d
Adrien Emery <adrien.emery@gmail.com>
2016-08-07 14:36:53 -0700

    add status endpoint

---
+++ auv_control/views.py
@@ -0,0 +1,17 @@
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import permissions, status
+
+
+class Status(APIView):
+    """
+    View to determine if service is up.
+    """
+    permission_classes = (permissions.AllowAny,)
+
+    def get(self, request, format=None):
+        """
+        Return a list of all users.
+        """
+        return Response(status=status.HTTP_200_OK,
+                        data={'status': 'ok'})
